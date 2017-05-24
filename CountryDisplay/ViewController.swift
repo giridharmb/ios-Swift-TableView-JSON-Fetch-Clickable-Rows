68382c7e0eed0986c88194a2d80b0e5ca3befecc
Giridhar Manchenahalli <gbhujan@l-idcm1ujg8w-m.idc.in.wal-mart.com>
2017-05-24 20:05:43 +0530

    Initial Commit

---
+++ CountryDisplay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CountryDisplay
+//
+//  Created by Giridhar Bhujanga on 24/05/17.
+//  Copyright © 2017 Giridhar Bhujanga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
